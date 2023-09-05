264acd1c42dbeefe6da415404845329fc0efb7e8
Theo <fanxiaobin422@sina.com>
2023-09-05 13:00:18 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: "https://github.com/vanhiupun/docs",
 
   author: {
     name: "Mr.Hope",
