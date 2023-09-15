9479dd27a05b82540eefaf5aee9c79978e2cca04
vanhiupun <fanxiaobin422@gmail.com>
2023-09-15 17:39:59 +0800

    单独导出sidebar和navbar

---
+++ docs/.vitepress/Sidebar.mjs
@@ -0,0 +1,123 @@
+export function getGuideSidebarZhCN() {
+  return [
+    {
+      text: "Netflix 观看指南",
+      collapsed: true,
+      items: [
+        { text: "Netflix 介绍", base: "/Netflix/", link: "js" },
+        { text: "Netflix 如何看4K及适配硬件", base: "/Netflix/", link: "yj" },
+        { text: "Netflix 常见使用问题及答案", base: "/Netflix/", link: "wt" },
+        {
+          text: "Netflix 常见错误提示及解决方案",
+          base: "/Netflix/",
+          link: "cw",
+        },
+        { text: "Netflix 秘密分类的技巧", base: "/Netflix/", link: "dh" },
+        { text: "Netflix 全球各国影视剧数量", base: "/Netflix/", link: "ys" },
+        { text: "Netflix 常用浏览器插件汇总", base: "/Netflix/", link: "cj" },
+        { text: "Netflix 总结", base: "/Netflix/", link: "zj" },
+      ],
+    },
+    {
+      text: "Disney+ 观看指南",
+      collapsed: true,
+      items: [
+        { text: "Disney+ 介绍", base: "/Disney/", link: "js" },
+        { text: "Disney+ 订阅指南", base: "/Disney/", link: "dy" },
+        { text: "Disney+ 合租介绍", base: "/Disney/", link: "hz" },
+        { text: "Disney+ 会员使用指南", base: "/Disney/", link: "hy" },
+        { text: "Disney+ 总结与建议", base: "/Disney/", link: "zj" },
+      ],
+    },
+    {
+      text: "Spotify Premium 指南",
+      collapsed: true,
+      items: [
+        {
+          text: "Spotify Premium 指南",
+          base: "/Spotify/",
+          link: "js",
+        },
+      ],
+    },
+    {
+      text: "YouTube Premium 会员权益及订阅指南",
+      collapsed: true,
+      items: [
+        {
+          text: "YouTube Premium 会员权益及订阅指南",
+          base: "/youtube/",
+          link: "js",
+        },
+      ],
+    },
+    {
+      text: "Hulu 国内观看指南",
+      collapsed: true,
+      items: [
+        {
+          text: "Hulu 国内观看指南",
+          base: "/hulu/",
+          link: "js",
+        },
+      ],
+    },
+    {
+      text: "HBO Max 国内观看指南",
+      collapsed: true,
+      items: [
+        {
+          text: "HBO Max 国内观看指南",
+          base: "/hbo/",
+          link: "js",
+        },
+      ],
+    },
+  ];
+}
+
+export function getCaseSidebarZhCN() {
+  return [
+    {
+      text: "科学上网指南",
+      collapsed: true,
+      base: "/kx/",
+      items: [
+        { text: "路由器与客户端选择", link: "ly" },
+        { text: "线路的区别与选择", link: "xl" },
+        { text: "关于机场常用服务器线路的科普", link: "kp" },
+      ],
+    },
+  ];
+}
+
+export function getDeveloperSidebarZhCN() {
+  return [
+    {
+      text: "流媒体合租平台",
+      collapsed: true,
+      base: "/hzpt/",
+      items: [
+        { text: "账号合租指南", link: "hz" },
+        { text: "蜜糖商店", link: "mtsd" },
+        { text: "奈飞小铺", link: "nfxp" },
+      ],
+    },
+  ];
+}
+
+export function getjcSidebarZhCN() {
+  return [
+    {
+      text: "推荐机场",
+      collapsed: true,
+      base: "/jc/",
+      items: [
+        { text: "解锁 4K Netflix 机场指南", link: "js" },
+        { text: "一元机场", link: "yy" },
+        { text: "薯条机场", link: "st" },
+        { text: "Bridge the Wise", link: "btw" },
+      ],
+    },
+  ];
+}
---
+++ docs/.vitepress/navbar.mjs
@@ -0,0 +1,98 @@
+export function lmt() {
+  return [
+    {
+      // 全方位流媒体观影指南
+      items: [
+        { text: "Netflix", link: "/Netflix/js" },
+        { text: "Disney+", link: "/Disney/js" },
+        { text: "Spotify会员", link: "/Spotify/js" },
+        { text: "YouTube会员", link: "/youtube/js" },
+        { text: "Hulu", link: "/hulu/js" },
+        { text: "HBO Max", link: "/hbo/js" },
+      ],
+    },
+  ];
+}
+
+export function hz() {
+  return [
+    {
+      // 🔥流媒体账号合租
+      items: [
+        { text: "账号合租指南", link: "/hzpt/hz" },
+        { text: "蜜糖商店", link: "/hzpt/mtsd" },
+        { text: "奈飞小铺", link: "/hzpt/nfxp" },
+      ],
+    },
+  ];
+}
+
+export function kx() {
+  return [
+    {
+      // 科学上网指南
+      items: [
+        { text: "路由器与客户端选择", link: "/kx/ly" },
+        { text: "线路的区别与选择", link: "/kx/xl" },
+        { text: "关于机场常用服务器线路的科普", link: "/kx/kp" },
+      ],
+    },
+  ];
+}
+
+export function jc() {
+  return [
+    {
+      // 推荐机场
+      items: [
+        { text: "解锁 4K Netflix 机场指南", link: "/jc/js" },
+        { text: "一元机场", link: "/jc/yy" },
+        { text: "薯条机场", link: "/jc/st" },
+        { text: "Bridge the Wise", link: "/jc/btw" },
+      ],
+    },
+  ];
+}
+
+export function fw() {
+  return [
+    // 推荐服务
+    {
+      text: "推荐机场",
+      items: [
+        {
+          text: "一元机场",
+          link: "https://xn--4gq62f52gdss.com/#/register?code=tFcXfJEu",
+        },
+        {
+          text: "薯条机场",
+          link: "https://av1.wtf/auth/register?code=vb4p",
+        },
+        {
+          text: "Bridge the Wise",
+          link: "https://patriot.ninja/aff.php?aff=1471",
+        },
+      ],
+    },
+
+    {
+      text: "流媒体合租平台",
+      items: [
+        {
+          text: "蜜糖商店",
+          link: "https://metshop.vip?referrerUserNo=MTU51076",
+        },
+        { text: "奈飞小铺", link: "https://ihezu.love/UKTer6" },
+      ],
+    },
+    {
+      text: "其他服务",
+      items: [
+        {
+          text: "SMS 海外接码平台",
+          link: "https://sms-activate.org/?ref=8170513",
+        },
+      ],
+    },
+  ];
+}
