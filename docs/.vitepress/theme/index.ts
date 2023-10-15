bb0ab726cc040c1009508f84dcf52c44dc2d879c
vanhiupun <fanxiaobin422@gmail.com>
2023-10-15 20:59:15 +0800

    添加图片放大功能

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,28 @@
 // .vitepress/theme/index.ts */
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
-import './vt-doc-custom-blocks.css'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
 
-export default DefaultTheme
+import './index.css'
+import './custom.css'
+import './custom-blocks.css'
+
+export default {
+  ...DefaultTheme,
+
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' })
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  }
+}
