fd7f76c722556dbd12158378d7131f2f00b7b9c0
Theo <fanxiaobin422@sina.com>
2023-10-19 12:11:29 +0800

    Variable (#19)
    * 🦄 refactor: 修改变量名,优化文件结构
    
    * 📃 docs: 添加注释

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -14,14 +14,21 @@ export default {
   ...DefaultTheme,
 
   setup() {
+    // 获取路由信息
     const route = useRoute()
+    // 初始化缩放
     const initZoom = () => {
+      // 设置图片缩放
       mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' })
+      // 设置图片缩放
       mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
     }
+    // 组件加载完成时执行
     onMounted(() => {
+      // 初始化缩放
       initZoom()
     })
+    // 监听路由变化，当路由变化时执行
     watch(
       () => route.path,
       () => nextTick(() => initZoom())
