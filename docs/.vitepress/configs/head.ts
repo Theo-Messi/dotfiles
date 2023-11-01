18eb54e837be07709ece25e7e1d697234db318ed
Theo <fanxiaobin422@sina.com>
2023-11-01 20:25:01 +0800

    🌈style : 更改主题配色/更新飞鸟机场活动 (#28)

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -5,7 +5,4 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 export const head: HeadConfig[] = [
   // LOGO
   ['link', { rel: 'icon', href: 'https://theovan.wiki/favicon.ico' }]
-
-  //强制开启为深色模式
-  // ["script", {}, `document.documentElement.classList.add('dark')`],
 ]
